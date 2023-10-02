528ab15aa76c404b670421e90942461e74d793eb
JuanMendozaOS <juanmendozasfa@gmail.com>
2023-10-02 20:41:50 -0300

    Add getAll Items

--- backend/models/item.js
+++ backend/models/item.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import dotenv from 'dotenv'
-import { getAuthor } from '../utils.js'
+import { countDecimals, getAuthor } from '../utils.js'
 dotenv.config()
 
 const BASE_URL = process.env.BASE_URL
@@ -13,19 +13,37 @@ export class ItemModel {
   static async getAll ({ query }) {
     const url = new URL(`${BASE_URL}/sites/MLA/search`)
     if (query) url.searchParams.set('q', query)
-    const responseData = (await axios.get(url)).data
+    const { data } = await axios.get(url)
 
-    const categories = responseData.filters.find(filter => filter.id === CATEGORY_ID)?.values
-      ?.map(category => category.name)
+    const categories = getCategories(data.filters)
+    const items = getItems(data.results)
 
-    console.log(categories)
-
-    if (!categories) throw new Error('No categories found')
-
-    return { author, ...responseData }
+    return { author, categories, items }
   }
 
   static async getById ({ id }) {
     throw new Error('Not implemented')
   }
+}
+
+function getCategories (filters) {
+  return filters.find(filter => filter.id === CATEGORY_ID)?.values
+    ?.map(category => category.name)
+}
+
+function getItems (results) {
+  return results.map(element => {
+    return {
+      id: element.id,
+      title: element.title,
+      price: {
+        currency: element.currency_id,
+        amount: element.price,
+        decimals: countDecimals(element.price)
+      },
+      picture: element.thumbnail,
+      condition: element.buying_mode,
+      free_shipping: element.shipping.free_shipping
+    }
+  })
 }
--- backend/utils.js
+++ backend/utils.js
@@ -6,3 +6,9 @@ export function getAuthor () {
 
   return author
 }
+
+export function countDecimals (number) {
+  if (Math.floor(number) === number) return 0
+
+  return number.toString().split('.')[1].length || 0
+}
