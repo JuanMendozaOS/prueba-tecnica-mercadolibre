b0c37472d00a9570e811539ec450f53ac342a72b
JuanMendozaOS <juanmendozasfa@gmail.com>
2023-10-04 21:16:09 -0300

    Add cors dependency

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 import express, { json } from 'express'
 import { createItemRouter } from './routes/items.js'
 import { ItemModel } from './models/item.js'
+import cors from 'cors'
 
 const app = express()
-app.use(json())
 
+app.use(json())
+app.use(cors())
 app.use('/api/items', createItemRouter({ itemModel: ItemModel }))
 
 const PORT = process.env.PORT ?? 3001
