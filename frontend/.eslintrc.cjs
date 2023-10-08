adfdfe884b26b7f9873b44cd7d47798efd6a9ba2
JuanMendozaOS <juanmendozasfa@gmail.com>
2023-10-08 12:42:12 -0300

    Add react-router-dom dependency

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,11 +1,11 @@
 module.exports = {
   root: true,
-  env: { node: true, es2020: true },
+  env: { browser: true, node: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'standard'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
